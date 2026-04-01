437f617e186c77635c18085e63fc41147faf156c
Nicoraclo <nicolas.diemunsch@viacesi.fr>
2026-04-01 14:12:49 +0200

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -67,17 +67,5 @@ return [
 
     
 
-    /*'logout' => ['controller' => AuthController::class, 'action' => 'logout'],
-    'deconnexion' => ['controller' => AccountController::class, 'action' => 'logoutConfirmation'],
-    'modification-compte-validation' => ['controller' => AccountController::class, 'action' => 'editValidation'],
-    'login' => ['controller' => AccountController::class,'action' => 'login'],
-    'logout' => ['controller' => AccountController::class, 'action' => 'logout'],
-    /*
-    'annonces' => ['controller' => OfferController::class, 'action' => 'list'],
-    'detail-annonce' => ['controller' => OfferController::class, 'action' => 'detail'],
-    'creation-compte-etudiant' => ['controller' => AccountController::class, 'action' => 'createStudent'],
-    'creation-compte-pilote' => ['controller' => AccountController::class, 'action' => 'createPilot'],
-    'creation-entreprise' => ['controller' => AccountController::class, 'action' => 'createCompany'],
-    'fiche-entreprise' => ['controller' => CompanyController::class, 'action' => 'detail'],
-    // Add all your other routes here... */
+
 ];
